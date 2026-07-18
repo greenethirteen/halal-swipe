95c5e39c7e2fb2bb2b2cc82b8f901a092eb37246
Abdullah Farouk <a.farouk9@gmail.com>
2026-07-18 17:35:49 +0400

    Revert "Move age range into popup selector"
    This reverts commit 5682f146131a40fa5b433ed769a1d9e96aa45e9b.

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,7 @@ class Settings(BaseSettings):
 
     @property
     def asset_version(self) -> str:
-        return f"{self.static_version}-20260718-09"
+        return f"{self.static_version}-20260718-08"
 
     @property
     def google_client_id_clean(self) -> str:
